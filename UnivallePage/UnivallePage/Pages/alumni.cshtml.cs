6720ca83e809ec437c9d0c0163e8e6d4cae3896e
SergioAIS <sergioiporresiles@gmail.com>
2025-09-08 22:58:28 -0400

    Pagina copia Univalle

---
+++ UnivallePage/UnivallePage/Pages/alumni.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnivallePage.Pages
+{
+    public class alumniModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ UnivallePage/UnivallePage/Pages/postgrado.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnivallePage.Pages
+{
+    public class postgradoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
