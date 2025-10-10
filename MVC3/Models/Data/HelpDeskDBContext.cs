d0863457ac22f02a0947c7ae21dec21fe1ffb9ac
SergioAIS <sergioiporresiles@gmail.com>
2025-10-10 15:33:53 -0400

    MVC3

---
+++ MVC3/Models/Data/HelpDeskDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC3.Models.Data
+{
+    public class HelpDeskDBContext: IdentityDbContext<IdentityUser>
+    {
+        public HelpDeskDBContext(DbContextOptions<HelpDeskDBContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ MVC3/Models/Mvc3Context.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC3.Models;
+
+public partial class Mvc3Context : DbContext
+{
+    public Mvc3Context()
+    {
+    }
+
+    public Mvc3Context(DbContextOptions<Mvc3Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-LBS8BBG;Initial Catalog=MVC3;Integrated Security=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.Property(e => e.LastName).HasDefaultValue("");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
